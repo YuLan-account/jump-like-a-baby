bd85f63dfee2bb29ebb97c19429e491fee6f1f90
zhongxuzhao <zhongxuzhao@bytedance.com>
2021-04-17 16:12:22 +0800

    update file

---
+++ app/build/generated/source/buildConfig/release/com/bytedance/jump/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bytedance.jump;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.bytedance.jump";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
